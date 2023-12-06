45dbe59c8d30b3f99e820db96af3a480c50ef3fb
Oleksandr Kachan <oleksandr.kachan@sigma.software>
2023-12-06 17:11:33 +0200

    Add request and error handlers

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module rssagg
 
 go 1.21
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/cors v1.2.1
+
+require github.com/go-chi/chi/v5 v5.0.10
--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello rssagg")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handleReadiness)
+	v1Router.Get("/error", handleError)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{Handler: router, Addr: ":" + portString}
+
+	fmt.Printf("Server starting on port: %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
